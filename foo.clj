607418c3a4cc1845f61fd0eaf29eeda023e6268d
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-08-22 13:46:59 +0300

    Remove old test file

--- foo.clj
+++
@@ -1,8 +0,0 @@
-(ns foo)
-
-(defn bar [event ctx]
-  (if (:virhe event)
-    (throw (RuntimeException. "ei onnistu"))
-    (merge event
-           {:success true
-            :jotain "ihan muuta"})))
